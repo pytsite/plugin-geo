69975da632f55df5c895e31d7d2632a7a1c6650c
Alexander Shepetko <a@shepetko.com>
2017-12-13 17:18:45 +0200

    Release 1.1

--- __init__.py
+++ __init__.py
@@ -1,24 +1,22 @@
 """PytSite Geo Package
 """
-# Public API
-from . import _widget as widget, _field as field, _validation_rule as validation_rule
-
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
 
+from pytsite import plugman as _plugman
 
-def __init():
-    """Init wrapper.
-    """
+if _plugman.is_installed(__name__):
+    # Public API
+    from . import _widget as widget, _field as field, _validation_rule as validation_rule
+
+
+def plugin_load():
     from pytsite import lang
     from plugins import assetman
 
     lang.register_package(__name__)
 
     assetman.register_package(__name__)
-    assetman.t_js(__name__ + '@**/*.js')
+    assetman.t_js(__name__)
     assetman.js_module('pytsite-geo-widget-location', __name__ + '@js/pytsite-geo-widget-location')
-
-
-__init()
